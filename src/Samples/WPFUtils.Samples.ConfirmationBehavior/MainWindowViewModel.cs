5aa9d57b3d52e5983bc411040c14d3bace5ad17d
Moshe Levi <moshejunkm@gmail.com>
2012-10-22 12:45:19 +0200

    ConfirmationBehavior added

---
+++ src/Samples/WPFUtils.Samples.ConfirmationBehavior/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.ViewModel;
+
+namespace WPFUtils.Samples.ConfirmationBehavior
+{
+    public class MainWindowViewModel : NotificationObject
+    {
+        private string m_info;
+
+        public string ConfirmationMessage
+        {
+            get { return "Message Here..."; }
+        }
+
+        public string ConfirmationCaption
+        {
+            get { return "Caption Here..."; }
+        }
+
+        public string Info
+        {
+            get { return m_info; }
+            set
+            {
+                if (m_info == value)
+                    return;
+
+                m_info = value;
+
+                RaisePropertyChanged(() => Info);
+            }
+        }
+
+        public DelegateCommand FirstCommand { get; private set; }
+        public DelegateCommand SecondCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            FirstCommand = new DelegateCommand(() => Info = "First Command Executed!");
+            SecondCommand = new DelegateCommand(() => Info = "Second Command Executed!");
+        }
+    }
+}
---
+++ src/WPFUtils/Behaviors/ConfirmationBehavior.cs
@@ -0,0 +1,106 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace WPFUtils.Behaviors
+{
+    public class ConfirmationBehavior<T> : Behavior<T> where T : ButtonBase
+    {
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof (string), typeof (ConfirmationBehavior<T>));
+
+        public string Message
+        {
+            get { return (string) GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public static readonly DependencyProperty CaptionProperty =
+            DependencyProperty.Register("Caption", typeof (string), typeof (ConfirmationBehavior<T>));
+
+        public string Caption
+        {
+            get { return (string) GetValue(CaptionProperty); }
+            set { SetValue(CaptionProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof (ICommand), typeof (ConfirmationBehavior<T>));
+
+        public ICommand Command
+        {
+            get { return (ICommand) GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register("CommandParameter", typeof (object), typeof (ConfirmationBehavior<T>));
+
+        public object CommandParameter
+        {
+            get { return GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Click += OnButtonClick;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.Click -= OnButtonClick;
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (Command == null || !Command.CanExecute(CommandParameter))
+                return;
+
+            if(!ShouldConfirm())
+            {
+                OnConfirmed();
+                return;
+            }
+
+            MessageBoxResult result = MessageBox.Show(Message, Caption, MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+                OnConfirmed();
+            else
+                OnNotConfirmed();
+        }
+
+        protected virtual bool ShouldConfirm()
+        {
+            return true;
+        }
+
+        protected virtual void OnConfirmed()
+        {
+            Command.Execute(CommandParameter);
+        }
+
+        protected virtual void OnNotConfirmed() { }
+    }
+
+    public class ConfirmationBehavior : ConfirmationBehavior<ButtonBase>
+    {
+        
+    }
+
+    public class ToggleConfirmationBehavior : ConfirmationBehavior<ToggleButton>
+    {
+        protected override bool ShouldConfirm()
+        {
+            return AssociatedObject.IsChecked == true;
+        }
+
+        protected override void OnNotConfirmed()
+        {
+            AssociatedObject.IsChecked = false;
+        }
+    }
+}
