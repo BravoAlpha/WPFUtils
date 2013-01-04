d8cdbdc448b60aad7d008f637d9a262ab67c686b
Moshe Levi <moshejunkm@gmail.com>
2013-01-04 23:27:20 +0200

    AlwaysOnTopBehavior added

---
+++ src/WPFUtils/Behaviors/AlwaysOnTopBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Interactivity;
+
+namespace WPFUtils.Behaviors
+{
+    public class AlwaysOnTopBehavior : Behavior<Window>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.LostFocus += (s, e) => AssociatedObject.Topmost = true;
+        }
+    }
+}
